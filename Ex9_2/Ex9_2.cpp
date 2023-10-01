bc9a08a8b79f5778cb49d38161ecdd44db2a70d1
kons2003 <mj030313@naver.com>
2023-10-01 21:09:16 +0900

    Ex9_2

--- Ex9_2/Ex9_2.cpp
+++ Ex9_2/Ex9_2.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 using namespace std;
 
+class Time {
+public:
+	Time(int hour = 0, int minute = 0, int second = 0) : hour_(hour), minute_(minute), second_(second) {}
+
+	Time operator+(const Time& time)
+	{
+		int hour = hour_ + time.hour_;
+		int minute = minute_ + time.minute_;
+		int second = second_ + time.second_;
+
+		if (second >= 60)
+		{
+			minute++;
+			second -= 60;
+		}
+		if (minute >= 60) 
+		{
+			hour++;
+			minute -= 60;
+		}
+		
+		return Time(hour, minute, second);
+	}
+	void Print() 
+	{
+		cout << hour_ << "½Ã " << minute_ << "ºÐ " << second_ << "ÃÊ" << endl;
+	}
+
+private:
+	int hour_;
+	int minute_;
+	int second_;
+};
+
 int main() {
+	Time time1(4, 50, 40);
+	Time time2(3, 40, 30);
+	Time time3 = time1 + time2;
+
+	time1.Print();
+	time2.Print();
+	time3.Print();
 
 	return 0;
 }
